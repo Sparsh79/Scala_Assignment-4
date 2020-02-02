55e33dfd1e6df7188eee2e488048dc337dc8036a
Sparsh <sparshbhardwaj10@gmail.com>
2020-02-02 16:16:17 +0530

    added the scala file of value class assignment

--- src/main/scala/edu/knoldus/com/value.scala
+++ src/main/scala/edu/knoldus/com/value.scala
@@ -1,5 +1,24 @@
 package edu.knoldus.com
 
-class value {
+class FirstName(val firstname: String) extends AnyVal{
+  def firstName = firstname
+}
+class SurName(val surName: String) extends AnyVal{
+  def lastName = surName
+}
+class Age(val age: Int) extends AnyVal{
+  def Age= age
+}
 
+object Value{
+  def main(args: Array[String]): Unit = {
+    val firstName = new FirstName(firstname = "Sparsh")
+    val surName = new SurName(surName = "Bhardwaj")
+    val age = new Age(age = 22)
+    display(firstName,surName,age)
+    def display(firstName: FirstName, surName: SurName, age: Age): Unit ={
+      println(firstName.firstName + " "+ surName.lastName + " is " + age.Age + " of age"  )
+
+    }
+  }
 }
